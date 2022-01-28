c77e2c943a3aac43352e72aa9de20d5b071b5616
amavr <albert.mavrin@gmail.com>
2022-01-28 16:41:36 +0300

    models

--- models/msg-06-01.js
+++ models/msg-06-01.js
@@ -1,12 +1,38 @@
 class SioMsg61 {
+
     constructor(data){
         this.sio_data = data;
         this.ise_data = null;
     }
 
-    loadIse(){
-        const str =  
-        this.ise_data = JSON.parse();
+}
+
+class SioAbn {
+
+    constructor(){
+        this.name = undefined;
+        this.inn = undefined;
+        this.dogs = [];
+    }
+
+}
+
+class SioDog {
+
+    constructor(){
+        this.num = undefined;
+        this.beg_dt = undefined;
+        this.end_dt = undefined;
+    }
+
+}
+
+class SioObj{
+
+    constructor(){
+        this.num = undefined;
+        this.beg_dt = undefined;
+        this.end_dt = undefined;
     }
 
 }
